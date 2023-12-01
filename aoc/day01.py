7f59baa118be57b10a004e9c03efb78a29492bfa
Peitao Ting <pting@Peitaos-MBP-M2.local>
2023-12-01 08:19:03 -0800

    Use replace() instead

--- aoc/day01.py
+++ aoc/day01.py
@@ -20,7 +20,7 @@ class Solver(aoc.util.Solver):
     def __init__(self, input: str):
         # sets self.input to the provided input
         super(Solver, self).__init__(input)
-        self.lines = [list(x) for x in self.input.splitlines()]
+        self.lines = self.input.splitlines()
         
 
     def part_one(self) -> int:
@@ -48,33 +48,9 @@ class Solver(aoc.util.Solver):
         sum = 0
         nums = [0, 0]
         for l in self.lines:
-            n = len(l)
-            for i, c in enumerate(l):
-                if c == 'o':
-                    if i + 2 < n and l[i+1] == 'n' and l[i+2] == 'e':
-                        l[i] = '1'
-                elif c == 't':
-                    if i + 2 < n and l[i+1] == 'w' and l[i+2] == 'o':
-                        l[i] = '2'
-                    elif i + 4 < n and l[i+1] == 'h' and l[i+2] == 'r' and l[i+3] == 'e' and l[i+4] == 'e':
-                        l[i] = '3'
-                elif c == 'f':
-                    if i + 3 < n and l[i+1] == 'o' and l[i+2] == 'u' and l[i+3] == 'r':
-                        l[i] = '4'
-                    elif i + 3 < n and l[i+1] == 'i' and l[i+2] == 'v' and l[i+3] == 'e':
-                        l[i] = '5'
-                elif c == 's':
-                    if i + 2 < n and l[i+1] == 'i' and l[i+2] == 'x':
-                        l[i] = '6'
-                    elif i + 4 < n and l[i+1] == 'e' and l[i+2] == 'v' and l[i+3] == 'e' and l[i+4] == 'n':
-                        l[i] = '7'
-                elif c == 'e':
-                    if i + 4 < n and l[i+1] == 'i' and l[i+2] == 'g' and l[i+3] == 'h' and l[i+4] == 't':
-                        l[i] = '8'
-                elif c == 'n':
-                    if i + 3 < n and l[i+1] == 'i' and l[i+2] == 'n' and l[i+3] == 'e':
-                        l[i] = '9'
-            
+            for a, b in self.map:
+                l = l.replace(a, b)
+
             for c in l:
                 if c.isdigit():
                     nums[0] = c
