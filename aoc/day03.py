cbee44a0cab3e807d08b81931e3a5c0c6b4822f0
Peitao Ting <pting@Peitaos-MBP-M2.local>
2023-12-06 10:52:39 -0800

    day03 use padding instead of checking bounds

--- aoc/day03.py
+++ aoc/day03.py
@@ -14,14 +14,18 @@ class Solver(aoc.util.Solver):
 
         # sets self.input to the provided input
         super(Solver, self).__init__(input)
-            
-        for r, row in enumerate(self.input.splitlines()):
+        lines = self.input.splitlines()
+        pad = "." * (len(lines[0]) + 2)
+        self.scem.append(pad)
+        for r, row in enumerate(lines):
+            row = "." + row + "."
             self.scem.append(row)
             for c, v in enumerate(row):
                 if not v.isdigit() and not v == '.':
-                    self.sym.append([r, c])
+                    self.sym.append([r+1, c])
                     if v == '*':
-                        self.stars.append([r, c])
+                        self.stars.append([r+1, c])
+        self.scem.append(pad)
         
     
     def getAdj(self, r, c):
@@ -30,36 +34,26 @@ class Solver(aoc.util.Solver):
                 (r+1, c-1), (r+1, c), (r+1, c+1)]
 
 
-    def inbounds(self, r, c):
-        return (r >= 0 and r < len(self.scem) and c >= 0 and c < len(self.scem[0]))
-
-
     def part_one(self) -> int:
         ret = 0
         seen = set()
         for row, col in self.sym:
             for r, c in self.getAdj(row, col):
-                if self.inbounds(r, c) and self.scem[r][c].isdigit() and (r, c) not in seen:
+                if self.scem[r][c].isdigit() and (r, c) not in seen:
                     partnum = []
                     seen.add((r, c))
                     # Traverse left and right
                     partnum.append(self.scem[r][c])
                     curr = c - 1
-                    while self.inbounds(r, curr):
-                        if self.scem[r][curr].isdigit():
-                            seen.add((r, curr))
-                            partnum.insert(0, self.scem[r][curr])
-                            curr -= 1
-                        else:
-                            break
+                    while self.scem[r][curr].isdigit():
+                        seen.add((r, curr))
+                        partnum.insert(0, self.scem[r][curr])
+                        curr -= 1
                     curr = c + 1
-                    while self.inbounds(r, curr):
-                        if self.scem[r][curr].isdigit():
-                            seen.add((r, curr))
-                            partnum.append(self.scem[r][curr])
-                            curr += 1
-                        else:
-                            break
+                    while self.scem[r][curr].isdigit():
+                        seen.add((r, curr))
+                        partnum.append(self.scem[r][curr])
+                        curr += 1
                     ret += int("".join(partnum))
         return ret
 
@@ -70,27 +64,21 @@ class Solver(aoc.util.Solver):
             seen = set()
             nums = []
             for r, c in self.getAdj(row, col):
-                if self.inbounds(r, c) and self.scem[r][c].isdigit() and (r, c) not in seen:
+                if self.scem[r][c].isdigit() and (r, c) not in seen:
                     partnum = []
                     seen.add((r, c))
                     # Traverse left and right
                     partnum.append(self.scem[r][c])
                     curr = c - 1
-                    while self.inbounds(r, curr):
-                        if self.scem[r][curr].isdigit():
-                            seen.add((r, curr))
-                            partnum.insert(0, self.scem[r][curr])
-                            curr -= 1
-                        else:
-                            break
+                    while self.scem[r][curr].isdigit():
+                        seen.add((r, curr))
+                        partnum.insert(0, self.scem[r][curr])
+                        curr -= 1
                     curr = c + 1
-                    while self.inbounds(r, curr):
-                        if self.scem[r][curr].isdigit():
-                            seen.add((r, curr))
-                            partnum.append(self.scem[r][curr])
-                            curr += 1
-                        else:
-                            break
+                    while self.scem[r][curr].isdigit():
+                        seen.add((r, curr))
+                        partnum.append(self.scem[r][curr])
+                        curr += 1
                     nums.append(int("".join(partnum)))
             if len(nums) == 2:
                 ret += nums[0] * nums[1]
