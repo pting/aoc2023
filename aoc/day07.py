d3356c50a551b2ba77b335884acfac441c7b7dc1
Peitao Ting <pting@Peitaos-MBP-M2.local>
2023-12-07 13:06:51 -0800

    Don't need to track highest card for poker hands

--- aoc/day07.py
+++ aoc/day07.py
@@ -9,35 +9,32 @@ class Solver(aoc.util.Solver):
     def __init__(self, input: str):
         # sets self.input to the provided input
         super(Solver, self).__init__(input)
-        self.lines = self.input.splitlines()
         record = []
-        record2 = []
 
-        for l in self.lines:
+        for l in self.input.splitlines():
             hand = l.split()
             n = int(hand[1])
-            record.append([self.score1(hand[0]), n])
-            record2.append([self.score2(hand[0]), n])
-        record.sort()
-        record2.sort()
+            record.append([hand[0], n])
 
+        record.sort(key=self.score1)
         self.ret1 = 0
         for i, [_, n] in enumerate(record):
             self.ret1 += (i+1) * n
 
+        record.sort(key=self.score2)
         self.ret2 = 0
-        for i, [_, n] in enumerate(record2):
+        for i, [_, n] in enumerate(record):
             self.ret2 += (i+1) * n
+
 
 
     order1 = {"A": 13, "K": 12, "Q": 11, "J": 10, "T": 9, "9": 8, "8": 7, "7": 6, "6": 5, "5": 4, "4": 3, "3": 2, "2": 1}
     order2 = {"A": 13, "K": 12, "Q": 11, "T": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2, "J": 1}
-    revorder2 = {13: "A", 12: "K", 11: "Q", 10: "T", 9: "9", 8: "8", 7: "7", 6: "6", 5: "5", 4: "4", 3: "3", 2: "2", 1: "J"}
 
-    def score1(self, hand):
+    def score1(self, s):
         temp = []
         count = {}
-        for c in hand:
+        for c in s[0]:
             temp.append(self.order1[c])
             if c not in count:
                 count[c] = 1
@@ -63,10 +60,11 @@ class Solver(aoc.util.Solver):
                 score += 1000000
         return score
 
-    def score2(self, hand):
+
+    def score2(self, s):
         temp = []
         count = {}
-        for c in hand:
+        for c in s[0]:
             temp.append(self.order2[c])
             if c not in count:
                 count[c] = 1
@@ -79,22 +77,21 @@ class Solver(aoc.util.Solver):
             score += n * i
             i *= 13
 
+        cards = []
+        for card, n in count.items():
+            if card != "J":
+                cards.append(n)
+        cards.sort(reverse=True)
         jokers = count["J"] if "J" in count else 0
-        if jokers and jokers < 5:
 
-            highcard = [0, ""]
-            for card, n in count.items():
-                if card != "J":
-                    highcard = max(highcard, [n, card])
-
-            if highcard[0] == 1:
-                temp.sort(reverse=True)
-                highcard = [1, self.revorder2[temp[0]]]
-            count[highcard[1]] += jokers
-            count["J"] = 0
-            
-        for n in count.values():
-            if n >= 5:
+        if jokers < 5:
+            cards[0] += jokers
+        else:
+            score += 20000000
+            return score
+                
+        for n in cards:
+            if n == 5:
                 score += 20000000
                 break
             if n == 4:
