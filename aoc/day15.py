831c8962f61839e61823d9aba90c9717448db67f
Peitao Ting <pting@Peitaos-MBP-M2.local>
2023-12-15 00:08:32 -0800

    Handle newlines

--- aoc/day15.py
+++ aoc/day15.py
@@ -9,45 +9,46 @@ import aoc.utilities
 class Solver(aoc.util.Solver):
     # Get list of all numbers or all words in input
     ret1, ret2 = 0, 0
-    focals = {}
     
     def __init__(self, input: str):
         # sets self.input to the provided input
-        super(Solver, self).__init__(input)        
-        self.steps = self.input.split(",")
+        super(Solver, self).__init__(input)
+        input = self.input.strip()
+        steps = input.split(",")
+
+        def myhash(s):
+            val = 0
+            for c in s:
+                val += ord(c)
+                val *= 17
+                val = val % 256
+            return val
 
         boxes = [[] for _ in range(256)]
         foc = -1
-        self.focals = {}
-        for s in self.steps:
-            self.ret1 += self.myhash(s)
+        focals = {}
+        for s in steps:
+            self.ret1 += myhash(s)
 
             if (l := s.find("-")) > 0:
                 label = s[:l]
-                h = self.myhash(label)
+                h = myhash(label)
                 if label in boxes[h]:
                     boxes[h].remove(label)
             else:
                 label, foc = s.split("=")
-                h = self.myhash(label)
+                h = myhash(label)
                 if label not in boxes[h]:
                     boxes[h].append(label)
-                self.focals[label] = foc
+                focals[label] = foc
 
         self.ret2 = 0
 
         for b, d in enumerate(boxes, 1):
             for i, label in enumerate(d, 1):
-                self.ret2 += b * i * int(self.focals[label])
+                self.ret2 += b * i * int(focals[label])
 
         
-    def myhash(self, s):
-        val = 0
-        for c in s:
-            val += ord(c)
-            val *= 17
-            val = val % 256
-        return val
         
 
     def part_one(self) -> int:
