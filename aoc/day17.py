fa49a650bf0eb91812fa989cfb78c55c56cab03e
Peitao Ting <pting@Peitaos-MBP-M2.local>
2023-12-19 04:35:01 -0800

    Stop if lower heat already found for a location

--- aoc/day17.py
+++ aoc/day17.py
@@ -3,6 +3,7 @@
 """17: PROBLEM NAME"""
 import aoc.util
 from heapq import heappush, heappop
+from collections import defaultdict
 
 # all solutions should subclass the `Solver` exposed by `aoc.util`
 # this class MUST be called Solver for the CLI discovery to work
@@ -20,17 +21,30 @@ class Solver(aoc.util.Solver):
     def minheat(self, start, least, most):
         queue = [(0, *start, 0,0)]
         seen = set()
+
+        def default_value():
+            return 1000000
+
+        costs = defaultdict(default_value)
+        # count = 0
+        
         while queue:
             heat, r, c, pc, pr = heappop(queue)
             if r == self.R - 1 and c == self.C - 1:
+                # print(count)
                 return heat
 
+            if heat > costs[(r, c)]:
+                # print(heat, costs[(r, c)])
+                # count += 1
+                continue
+            
             if (r, c, pc, pr) in seen:
                 continue
             seen.add((r, c, pc, pr))
 
             # calculate turns only
-            for dr, dc in {(1,0), (0,1), (-1,0), (0,-1)} - {(pc,pr), (-pc,-pr)}:
+            for dr, dc in {(1, 0), (0, 1), (-1, 0), (0, -1)} - {(pc, pr), (-pc, -pr)}:
                 nr, nc, h = r, c, heat
                 # go straight
                 for i in range(1, most + 1):
@@ -38,6 +52,7 @@ class Solver(aoc.util.Solver):
                     if 0 <= nr < self.R and 0 <= nc < self.C:
                         h += self.grid[nr][nc]
                         if i >= least:
+                            costs[(nr, nc)] = h
                             heappush(queue, (h, nr, nc, dr, dc))
 
 
